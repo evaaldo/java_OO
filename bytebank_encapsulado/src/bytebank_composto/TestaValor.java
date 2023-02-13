11c3f925333259553bf3d85801dc102b37df1e26
evaaldo <evaaldormj@gmail.com>
2023-02-13 00:12:32 -0300

    Feat: static e construtor

---
+++ bytebank_encapsulado/src/bytebank_composto/TestaValor.java
@@ -0,0 +1,14 @@
+package bytebank_composto;
+
+public class TestaValor {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta(1337, 24666);		
+		Conta conta2 = new Conta(1338, 25666);
+		Conta conta3 = new Conta(1339, 25667);
+		
+		
+	}
+	
+}
